404e6f203b5dd666a9ac1da5a6734ef2d94fba61
eunho_Lee <pixeed@naver.com>
2022-04-08 02:06:48 +0900

    프로그래머스 level1_로또의 최고 순위와 최저 순위
    https://programmers.co.kr/learn/courses/30/lessons/77484

--- algorithm/algorithm/level_1_lotto.c
+++ algorithm/algorithm/level_1_lotto.c
@@ -1,45 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define NUM 6
 
-void main(void) {
-
-	int lottos[NUM];
-	int win_nums[NUM];
-
-	int lottos_len = sizeof(lottos)/sizeof(int);
-	int win_nums_len = sizeof(win_nums) / sizeof(int);
-	//printf("%d", win_nums_len);
-
-	//민우의 로또 번호 (0~45)
-	for (int i = 0; i < lottos_len; i++) {
-		lottos[i] = rand() % 46;
-	}
-	//정답 로또 번호 (1~45)
-	for (int i = 0; i < win_nums_len; i++) {
-		win_nums[i] = rand() % 45 + 1;
-	}
-
-	printf("lottos : ");
-	for (int i = 0; i < lottos_len; i++) {
-		printf("%d ", lottos[i]);
-	}
-	printf("\nwin_num : ");
-	for (int i = 0; i < win_nums_len; i++) {
-		printf("%d ", win_nums[i]);
-	}
-
-	/*
-	int answer[2];
-
-	answer[0], answer[1] = solution(lottos, lottos_len, win_nums, win_nums_len);
-
-	printf("\nBest %d등", answer[0]);
-	printf("\nWorst %d등", answer[1]);
-	*/
-	
-}
 
 int* solution(int lottos[], size_t lottos_len, int win_nums[], size_t win_nums_len) {
 	int* answer = (int*)malloc(sizeof(int) * 2);
@@ -60,8 +24,56 @@ int* solution(int lottos[], size_t lottos_len, int win_nums[], size_t win_nums_l
 		}
 	}
 
-	//answer[0] = arr[cnt + zeros];
-	//answer[1] = arr[cnt];
+	answer[0] = arr[cnt + zeros];
+	answer[1] = arr[cnt];
 
 	return answer;
+}
+
+void main(void) {
+
+	int lottos[NUM];
+	int win_nums[NUM];
+
+	size_t lottos_len = sizeof(lottos)/sizeof(int);
+	size_t win_nums_len = sizeof(win_nums) / sizeof(int);
+	//printf("%d", win_nums_len);
+	srand(time(NULL));
+
+	//민우의 로또 번호 (0~45)
+	for (int i = 0; i < lottos_len; i++) {
+		lottos[i] = rand() % 46;
+		for (int k = 0; k < i; k++) {
+			if (lottos[k] == lottos[i] && lottos[i] != 0) {
+				i--;
+				break;
+			}
+		}
+	}
+	//정답 로또 번호 (1~45)
+	for (int i = 0; i < win_nums_len; i++) {
+		win_nums[i] = rand() % 45 + 1;
+		for (int k = 0; k < i; k++) {
+			if (win_nums[k] == win_nums[i]) {
+				i--;
+				break;
+			}
+		}
+	}
+
+	printf("lottos : ");
+	for (int i = 0; i < lottos_len; i++) {
+		printf("%d ", lottos[i]);
+	}
+	printf("\nwin_num : ");
+	for (int i = 0; i < win_nums_len; i++) {
+		printf("%d ", win_nums[i]);
+	}
+
+	
+
+	 int* answer = solution(lottos, lottos_len, win_nums, win_nums_len);
+	 
+	 printf("\n Best : %d \n Worst : %d", answer[0], answer[1]);
+	
 }
